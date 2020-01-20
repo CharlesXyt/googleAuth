229f427bb547c9d3844523f387e0003d09172a58
charlesxyt <charles.yitong@gmail.com>
2020-01-20 16:36:31 +1100

    Updates

---
+++ precache-manifest.1314edc91078a18c30b0e7531e53c2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c8d7b0d52a72f59942add2871123a7",
+    "url": "/google-award/index.html"
+  },
+  {
+    "revision": "9612787409708a3b5c95",
+    "url": "/google-award/static/css/main.df9353e5.chunk.css"
+  },
+  {
+    "revision": "b4ba0e796fecb689dd60",
+    "url": "/google-award/static/js/2.4d221d1d.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/google-award/static/js/2.4d221d1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9612787409708a3b5c95",
+    "url": "/google-award/static/js/main.9b86b2f5.chunk.js"
+  },
+  {
+    "revision": "4fa1ce32ebd1bc0ebb18",
+    "url": "/google-award/static/js/runtime-main.7f863880.js"
+  }
+]);
